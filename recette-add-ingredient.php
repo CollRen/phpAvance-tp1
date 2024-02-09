87ef48dfe7e08de68a519e9d0d7c78021006a05e
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-02-08 22:08:23 -0500

    Permet le insert vers la bd

--- recette-add-ingredient.php
+++ recette-add-ingredient.php
@@ -1,25 +1,49 @@
-
 <?php
-/* if(isset($_GET['id']) && $_GET['id']!=null){
+
+if(isset($_GET['id']) && $_GET['id']!=null){
     require_once('classes/Recette.php');
     $recette = new Recette;
+    $id = $_GET['id'];
     $selectId = $recette->selectId('recettes.recette', $_GET['id'], 'index');
     extract($selectId);
 }else{
     header('location:index.php');
 }
-$tableName = 'recettes.recette';
-$value = $id;
-$url = 'index'; */
+print_r($_POST);
+echo '<br>';
+echo '<br>';
+echo '<br>';
+$arrayToInsert = array();
 
+if(isset($_POST['ingredient_id'])){
 
-    if(isset($_POST['nom'])){
-        require('./classes/Recette-has-ingredient.php');
-        $recetteHasIng = new RecetteHasIngredient();
-        $action = $recetteHasIng->insert('recettes.recette_has_ingredient', $_POST);
+    for ($i = 0; $i < 7; $i++) { 
+        if($_POST['quantite'][$i] > 0 ) {
+            $arrayToInsert['recette_id'] = $id;
+            $arrayToInsert['ingredient_id'] = $_POST['ingredient_id'][$i];
+            $arrayToInsert['quantite'] = $_POST['quantite'][$i];
+            $arrayToInsert['unite_mesure_id'] = $_POST['unite_mesure_id'][$i];
+            require_once('./classes/Recette-has-ingredient.php');
+            $recetteHasIngredient = new RecetteHasIngredient;
+            $recetteHasIngredient->setProp($arrayToInsert);
+        }
     }
-?> 
+    
 
+
+/*     require_once('./classes/Recette-has-ingredient.php');
+    $hasIngredient = new RecetteHasIngredient();
+    $action = $hasIngredient->setProp($_POST); */
+    } else {
+        echo 'marche pas';
+    }
+
+/*         require('./classes/Recette-has-ingredient.php');
+        $recetteHasIng = new RecetteHasIngredient();
+        $insert = $action = $recetteHasIng->insert('recettes.recette_has_ingredient', $_POST);
+        header("location:recette-has-ingredient-insert.php" . $insert); 
+    }*/
+?> 
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -31,14 +55,14 @@ $url = 'index'; */
 </head>
 <body>
 <?php include('./menu.php');?>
-
     <div class="container">
         <h2>Recette add ingredient Create</h2>
 
-        <form action="<?php $action  ?>" method="post">
+        <form action="<?php $action ?>" method="post">
             <table>
                 <thead>
                     <tr>
+                        <td>#</td>
                         <td>Ingrédients</td>
                         <td>Qté</td>
                         <td>U. Mesure</td>
@@ -49,22 +73,25 @@ $url = 'index'; */
                     <tr>
                         <?php require_once('./classes/Ingredient.php'); 
                         $ing = new ingredient;
-                        $ing = $ing->getListeingredient(); 
+                        $ing = $ing->select('recettes.ingredient'); 
                         foreach ($ing as $row) { ?>
-
                         <td>
+                        <input type="text" name="ingredient_id[]" value="<?php echo $row['id']; ?>"/>
+                        </td>
+                        <td>
+                       
                             <?php echo $row['nom']; ?>
                         </td>
 
                         <td>
-                            <input max="10" min="0" name="qte[]" step=".25" type="number" value="0" />
+                            <input max="10" min="0" name="quantite[]" step=".25" type="number" value="0" />
                         </td>
                         
                         <td>
-                            <select name="UMesure[]">
+                            <select name="unite_mesure_id[]">
                                 <?php require_once('./classes/UMesure.php'); 
                                 $Umesure = new UMesure;
-                                $Umesure = $Umesure->getListeUMesure();
+                                $Umesure = $Umesure->select('recettes.unite_mesure');
                                 foreach ($Umesure as $row) { ?>
                                 
                                     <option value="<?php echo $row['id']; ?>"><?php echo $row['nom']; ?></option>
